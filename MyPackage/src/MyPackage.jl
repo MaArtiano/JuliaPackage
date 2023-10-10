c6935cb0bfc9ba4bc622ab518e1fa0cfe7fb1d77
MaArtiano <martiano@uni-mainz.de>
2023-10-10 10:17:08 +0200

    inizio# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master## Initial commit## Changes to be committed:#	new file:   MyPackage/Manifest.toml#	new file:   MyPackage/Project.toml#	new file:   MyPackage/src/MyPackage.jl#

---
+++ MyPackage/src/MyPackage.jl
@@ -0,0 +1,57 @@
+module MyPackage
+
+function rse_sum(x)
+
+    return +(x...)
+    
+    end
+    
+    function rse_mean(x; Σ = rse_sum(x), L = length(x))
+    
+    return Σ/L
+    
+    end
+    
+    function rse_std(x,Mean = rse_mean(x), L = length(x))
+    
+    return sqrt.(rse_sum((x.-Mean).^2)/(L-1))
+    
+    end
+    
+    
+    # With the dot comma we are introducing options, if we put it explicitily then he's gonna use the one we used, otherwise he's gonna evaluate it as we said it to be.
+    function rse_tstat(x; σ = rse_std(x),L = length(x), Mean = rse_mean(x))
+    
+    return (Mean/σ).*sqrt(L)
+    
+    end
+    
+    
+    struct StatResult
+    x
+    n
+    std
+    tvalue
+    end
+    
+    function StatResult(x)
+    
+    return StatResult(x,length(x),rse_std(x),rse_tstat(x))   
+    
+    end
+    
+    function print(s::StatResult)
+        println("The vector is the following:",lenght(s.x))
+        println("Its length is: ",length(s.n))
+        println("Std of x is:", length(s.std))
+        println("Tvalues are:",length(s.tvalue))
+    end    
+    
+    #export rse_sum
+    #export rse_mean
+    #export rse_std
+    #export rse_stat
+    #export print
+    #export StatResult
+
+end # module MyPackage
